8660376218fa711b7cc08ceded7adf89f116449e
Sarang choudhary <134210561+Sarang0045@users.noreply.github.com>
2026-07-03 15:14:20 +0530

    Time: 4 ms (67.19%), Space: 94.3 MB (79.49%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,19 +1,47 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        StringBuilder sb = new StringBuilder();
-        while (head != null) {
-            sb.append(head.val);
-            head = head.next;
+
+        if (head == null || head.next == null)
+            return true;
+
+        // Step 1: Find the middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        String s = sb.toString();
-        int i = 0;
-        int j = s.length() - 1;
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j))
+
+        // Step 2: Reverse the second half
+        ListNode secondHalf = reverse(slow.next);
+
+        // Step 3: Compare both halves
+        ListNode firstHalf = head;
+        ListNode temp = secondHalf;
+
+        while (temp != null) {
+            if (firstHalf.val != temp.val)
                 return false;
-            i++;
-            j--;
+
+            firstHalf = firstHalf.next;
+            temp = temp.next;
         }
+
         return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
     }
 }
